ddb4b1f21e41ff0d4aeafba0c5590beb5e903f35
LenZuro <126126939+LenZuro@users.noreply.github.com>
2023-09-05 21:45:29 -0700

    final fixes before push

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
+import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import {
   ApolloClient,
@@ -35,16 +33,12 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
+      
         <>
           <Navbar />
-          <Switch>
-            <Route exact path="/" component={SearchBooks} />
-            <Route exact path="/saved" component={SavedBooks} />
-            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
-          </Switch>
+        <Outlet/>
         </>
-      </Router>
+     
     </ApolloProvider>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const { ApolloServer } = require("apollo-server-express");
+const { ApolloServer }  = require("@apollo/server");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require('./utils/auth');
+const { expressMiddleware } = require('@apollo/server/express4')
 
 const app = express();
 const server = new ApolloServer({
@@ -12,27 +13,27 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 const PORT = process.env.PORT || 3001;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.json(__dirname , '../client/build')));
+  app.use(express.static(path.join(__dirname , '../client/dist')));
 }
 
-app.get('/' , (req ,res) => {
-  res.sendFile(path.json(__dirname , '../client'))
+app.get('*' , (req ,res) => {
+  res.sendFile(path.join(__dirname , '../client/dist/index.html'))
 })
 
 const startApolloServer = async () => {
   await server.start();
-  server.applyMiddleware({ app });
-
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use('/graphql' , expressMiddleware(server));
   db.once('open' , () => {
     app.listen(PORT , () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
   })
 };
+
 startApolloServer();
